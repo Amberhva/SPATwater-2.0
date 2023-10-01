14641026ce69d3c0849c965033f2360491699709
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-10-01 14:30:43 +0200

    Sveltekit start + parallax werkend

---
+++ src/lib/components/Over.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+
+<!--==================== ABOUT ====================-->
+<section class="about section" id="about">
+  <div class="about__container container grid">
+    <div class="about__data">
+      <h2 class="section__title about__title">Over SPATwater</h2>
+      <p class="about__description">
+        SPATwater bestaat uit een nieuwe generatie enthousiaste hydrologen die
+        met een technische achtergrond nét wat anders kijken naar ruimtelijke
+        opgaven. De uitdagingen zijn enorm, maar de kansen die het biedt nog
+        groter. <br />
+        <br />Met expertise op het gebied van waterkwaliteit, de Kaderrichtlijn
+        Water en klimaatadaptatie helpt SPATwater overheden en bedrijven om
+        toekomstbestendig en waterrobuust te worden.
+        <br />
+        <br />
+        Wij voeren geen analyse uit zonder handelingsperspectief en bieden zowel
+        creatieve als praktische oplossingen.
+      </p>
+      <a href="#expertise" class="button">
+        <p class="button_text">Onze expertise</p>
+      </a>
+    </div>
+
+    <div class="about__img">
+      <div class="about__img-overlay">
+        <img src="assets/img/tree.png" alt="" class="about__img-one" />
+      </div>
+    </div>
+  </div>
+</section>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Navbar from "../lib/components/Navbar.svelte";
+  import Home from "../lib/components/Home.svelte";
+  import Over from "../lib/components/Over.svelte";
+</script>
+
+<Navbar />
+<Home />
+<Over />
