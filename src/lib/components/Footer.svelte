dadea364928608d8f33d91960e8a43e0f1d89513
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-10-01 16:38:59 +0200

    Omgezet naar Sveltekit

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<section>
+  <h2>Footer</h2>
+</section>
+
+<style>
+  h2 {
+    padding: 4rem 2.5rem;
+    color: var(--spat);
+  }
+
+  section {
+    height: 100vh;
+    background: var(--lg-bg);
+  }
+</style>
