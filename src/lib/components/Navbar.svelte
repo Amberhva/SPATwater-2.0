cc55727ced868bad732e9759cb6afb3c06528141
Tjips <thomas@scheepers.com>
2023-10-01 18:08:56 +0200

    projecten linken

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -26,7 +26,7 @@
         <li class="nav-link"><a href="#">Home</a></li>
         <li class="nav-link"><a href="#">Over</a></li>
         <li class="nav-link"><a href="#">Expertise</a></li>
-        <li class="nav-link"><a href="#">Projecten</a></li>
+        <li class="nav-link"><a href="#projecten">Projecten</a></li>
         <li class="nav-link"><a href="#">Blog</a></li>
         <li class="nav-link"><a href="#">Team</a></li>
         <li class="nav-link"><a href="#"><span>Contact</span></a></li>
