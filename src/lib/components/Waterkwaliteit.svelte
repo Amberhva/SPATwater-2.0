d8c969647f9fea7ae40c1fee9fcf80a0e41c2e98
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-10-05 09:51:16 +0200

    Waterkwaliteit mobiel

--- src/lib/components/Waterkwaliteit.svelte
+++ src/lib/components/Waterkwaliteit.svelte
@@ -2,17 +2,65 @@
 </script>
 
 <section>
-  <h2>Waterkwaliteti</h2>
+  <h2>Waterkwaliteit</h2>
+  <span><strong>Wat betekent de Kaderrichtlijn Water voor jou?</strong></span>
+<p class="waterkwaliteit_description">De Kaderrichtlijn Water (KRW) is een Europese richtlijn die regelt dat oppervlaktewateren en het grondwater van voldoende kwaliteit zijn. Toch voldoen pas enkele wateren aan deze norm. We staan nu op een keerpunt. Binnen 5 jaar, in 2027, moeten alle oppervlaktewateren en ons grondwater voldoen aan de KRW. Vergelijkingen met de stikstofcrisis zijn snel gemaakt, maar de KRW biedt ook kansen in proposities, aanbestedingen en vergunningverlening.
+SPATwater biedt hulp aan organisaties bij het identificeren van de implicaties van de KRW voor hun specifieke situatie. Dit doen wij onder andere door middel van:
+</p>
+
+<p class="arrow_section">
+<br><svg xmlns="http://www.w3.org/2000/svg" width="22" height="12" viewBox="0 0 22 12" fill="none">
+  <path d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z" fill="black"/>
+</svg> Het verzorgen van kennissessies
+
+<br><svg xmlns="http://www.w3.org/2000/svg" width="22" height="12" viewBox="0 0 22 12" fill="none">
+  <path d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z" fill="black"/>
+</svg> Analyse van huidige KRW-risico’s en kansen
+
+<br><svg xmlns="http://www.w3.org/2000/svg" width="22" height="12" viewBox="0 0 22 12" fill="none">
+  <path d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z" fill="black"/>
+</svg> Propositieverbetering
+</p>
+
+<img src="assets/Aquathermie.png" alt="foto aquathermie" class="img_aquathermie">
+
 </section>
 
 <style>
   h2 {
-    padding: 4rem 2.5rem;
-    color: var(--spat);
+ color: var(--spat);
+    font-size: 1.4rem;
+    margin-bottom: 0.5rem;
   }
 
   section {
-    height: 100vh;
-    background: white;
+height: 100vh;
+    padding: 5rem 2.5rem;
+    overflow: hidden;
   }
+
+span{
+  color: var(--darkblue);
+
+}
+
+p.waterkwaliteit_description, .arrow_section{
+    color: var(--darkblue);
+    display: block;
+    font-size: 0.8rem;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+}
+
+svg{
+width: 21px;
+flex-shrink: 0;
+stroke-width: 1.5px;
+}
+
+.img_aquathermie{
+transition: 0.3s;
+    width: 100%;
+}
+
 </style>
