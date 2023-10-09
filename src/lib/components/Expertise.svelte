2098aa1bcaedf74964dc56b62573da9aca31d3d7
zenitba <112856019+zenitba@users.noreply.github.com>
2023-10-09 20:46:27 +0200

    klimaat&expertise
    klaar alleen nog logo's

--- src/lib/components/Expertise.svelte
+++ src/lib/components/Expertise.svelte
@@ -16,6 +16,17 @@
   <div
     class="expertise-container"
   >
+    <!-- Afbeelding voor Klimaatadaptatie -->
+    <div
+      class="right"
+    >
+      <img
+        class="expertise-img"
+        src="assets/Untitled4.png"
+        style="width: 400px; height: 250px;"
+        alt="Illustratie Klimaat"
+      />
+    </div>
     <!-- Expertise-info voor Klimaatadaptatie -->
     <div
       class="expertise-info"
@@ -58,32 +69,12 @@
         </p>
       </a>
     </div>
-    <!-- Afbeelding voor Klimaatadaptatie -->
-    <div
-      class="right"
-    >
-      <img
-        class="expertise-img"
-        src="assets/Untitled4.png"
-        style="width: 350px; height: 200px;"
-        alt="Illustratie waterkwaliteit"
-      />
-    </div>
   </div>
 
   <!-- Tweede Expertise-container -->
   <div
     class="expertise-container"
   >
-    <!-- Afbeelding voor Waterkwaliteit -->
-    <div>
-      <img
-        class="expertise-img"
-        src="assets/Untitled3.png"
-        style="width: 350px; height: 200px;"
-        alt="Illustratie Klimaat"
-      />
-    </div>
     <!-- Expertise-info voor Waterkwaliteit -->
     <div
       class="expertise-info left"
@@ -124,12 +115,32 @@
         </p>
       </a>
     </div>
+    <!-- Afbeelding voor Waterkwaliteit -->
+    <div>
+      <img
+        class="expertise-img"
+        src="assets/Untitled3.png"
+        style="width:500px; height: 250px;"
+        alt="Illustratie Waterkwaliteit"
+      />
+    </div>
   </div>
 
   <!-- Derde Expertise-container -->
   <div
     class="expertise-container"
   >
+    <!-- Afbeelding voor B-RAIN -->
+    <div
+      class="right2"
+    >
+      <img
+        class="expertise-img"
+        src="assets/brainlogo.png"
+        style="width: 400px; height:250px;"
+        alt="Illustratie B-RAIN"
+      />
+    </div>
     <!-- Expertise-info voor B-RAIN -->
     <div
       class="expertise-info right"
@@ -156,9 +167,11 @@
         houden van
         hun tuin.
       </p>
+      <br />
+      <br />
       <!-- Link naar meer informatie -->
       <a
-        href="#waterkwaliteit"
+        href="#b-Rain"
         class="button"
       >
         <!-- Tekst voor de link -->
@@ -169,45 +182,28 @@
         </p>
       </a>
     </div>
-    <!-- Afbeelding voor B-RAIN -->
-    <div
-      class="right2"
-    >
-      <img
-        class="expertise-img"
-        src="assets/brainlogo.png"
-        style="width: 350px; height: 200px;"
-        alt="Illustratie B-RAIN"
-      />
-    </div>
   </div>
 </section>
 
 <style>
   /* Stijlen voor de Expertise sectie */
   .expertise-section {
-    padding: 4rem
-      2.5rem;
+    padding: 3rem
+      4rem;
     color: var(
       --spat
     );
   }
-
   /* Globale stijlen voor afbeeldingen */
   img {
     max-width: 400px;
-    border-radius: 20px;
   }
-
   /* Stijlen voor de Expertise sectie container */
   .expertise {
-    background: white;
-    padding: 30px
-      50px;
-    border-radius: 20px;
+    padding: 3rem
+      4rem;
     height: 100%;
   }
-
   /* Stijlen voor de Expertise container */
   .expertise-container {
     display: flex;
@@ -215,44 +211,36 @@
     margin-bottom: 2rem;
     align-items: left;
   }
-
   /* Stijlen voor afbeeldingen binnen de Expertise container */
   .expertise-img {
     max-width: 400px;
-    border-radius: 20px;
     max-width: 100%;
     height: auto;
   }
-
   /* Stijlen voor rechterzijde binnen de Expertise container */
   .right {
     margin-left: 1rem;
   }
-
   /* Stijlen voor rechterzijde binnen de Expertise container */
   .right2 {
     margin-right: 1rem;
   }
-
   /* Stijlen voor informatie binnen de Expertise container */
   .expertise-info {
     display: grid;
     place-content: center;
   }
-
   /* Stijlen voor linkerzijde binnen de Expertise container */
   .left {
     margin-left: 3rem;
   }
-
   /* Stijlen voor de Expertise header */
   .expertise-header {
     color: var(
       --spat
     );
-    font-size: 2rem;
+    font-size: 1.5rem;
   }
-
   /* Stijlen voor de Expertise titel */
   .expertise-title {
     margin-top: 10px;
@@ -260,21 +248,19 @@
     color: var(
       --spat
     );
-    font-size: 1.4rem;
+    font-size: 1.2rem;
     font-weight: bold;
   }
-
   /* Stijlen voor de Expertise omschrijving */
   .omschrijving {
-    max-width: 500px;
+    max-width: 450px;
     font-size: 0.8rem;
     color: var(
       --darkblue
     );
     margin-bottom: 0.5rem;
-    text-align: justify;
+    text-align: start;
   }
-
   /* Stijlen voor de knop */
   .button {
     display: inline-block;
@@ -282,24 +268,20 @@
       --green
     );
     color: white;
-    padding: 0.7rem
-      1rem;
     border-radius: 5px;
     font-weight: var(
       --font-semi-bold
     );
+    width: fit-content;
+    height: 45px;
     transition: 0.3s;
-    text-decoration: none;
-    width: 120px;
-    margin-top: 10px;
-    font-size: 1rem;
   }
-
   /* Stijlen voor de knoptekst */
   .button_text2 {
+    color: white;
     text-decoration: none;
+    font-size: 0.9rem;
   }
-
   /* Responsieve stijlen voor schermen kleiner dan 768px */
   @media only screen and (max-width: 768px) {
     /* Aanpassingen voor Expertise-container */
@@ -308,20 +290,17 @@
       flex-direction: column;
       text-align: center;
     }
-
     /* Aanpassingen voor Expertise-info */
     .expertise-info {
       margin-bottom: 1rem;
       order: 2;
     }
-
     /* Aanpassingen voor Expertise-afbeeldingen */
     .expertise-img {
       order: 1;
       max-width: 100%;
       height: auto;
     }
-
     /* Aanpassingen voor rechter- en linkerzijde binnen de Expertise container */
     .right,
     .left {
