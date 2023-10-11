90d1946b808bb6441e7f281e9a30fef2374401c7
Tjips <thomas@scheepers.com>
2023-10-11 20:06:05 +0200

    Added background color

--- src/lib/components/Klimaatadaptatie.svelte
+++ src/lib/components/Klimaatadaptatie.svelte
@@ -3,61 +3,63 @@
 
 <!-- Klimaat section -->
 
-<section class="first-section">
-  <div class="inner-section">
-    <div class="horizontal-flex-row">
-      <div class="text-container">
-        <h2>Klimaatadaptatie</h2>
-        <p class="bold-text">Als meer dan 60% van een gemeente private grond is… dan is het noodzakelijk om deze grond klimaatadaptief te maken.</p>
-        <p>Ons vertrekpunt is dat een groot deel van een gemeente bestaat uit private grond (tuinen, daken, bedrijventerreinen) en dat het absoluut noodzakelijk is dat deze private ruimte klimaatadaptief ingericht moet worden. Elk perceel vraagt om maatwerk, maar het is onmogelijk om elke tuin in te gaan.</p>
-        <ul>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe los je dit probleem op?</li>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe zet je subsidiestromen efficiënt in?</li>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe activeer je bewoners?</li>
-        </ul>
-      </div>
-      <div class="image-container">
-        <img src="assets/projects1.png" alt="">
+<div class="light-background">
+  <section class="first-section">
+    <div class="inner-section">
+      <div class="horizontal-flex-row">
+        <div class="text-container">
+          <h2>Klimaatadaptatie</h2>
+          <p class="bold-text">Als meer dan 60% van een gemeente private grond is… dan is het noodzakelijk om deze grond klimaatadaptief te maken.</p>
+          <p>Ons vertrekpunt is dat een groot deel van een gemeente bestaat uit private grond (tuinen, daken, bedrijventerreinen) en dat het absoluut noodzakelijk is dat deze private ruimte klimaatadaptief ingericht moet worden. Elk perceel vraagt om maatwerk, maar het is onmogelijk om elke tuin in te gaan.</p>
+          <ul>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe los je dit probleem op?</li>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe zet je subsidiestromen efficiënt in?</li>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Hoe activeer je bewoners?</li>
+          </ul>
+        </div>
+        <div class="image-container">
+          <img src="assets/projects1.png" alt="">
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section>
-  <div class="inner-section">
-    <div class="horizontal-flex-row">
-      <div class="image-container">
-        <img class="mobile-image" src="assets/projects4.png" alt="">
-      </div>
-      <div class="text-container">
-        <h2>Gemeenten</h2>
-        <p class="bold-text">Wij kunnen elke bewoner in uw gemeente een persoonlijk klimaatadaptief advies geven zonder elke tuin in te gaan.</p>
-        <p>Wij gebruiken ruimtelijke data en sociale kennis uit de buurt om de gemeente gedetailleerd in kaart te brengen. In combinatie met veldwerk kunnen wij op postcode, buurt of wijkniveau klimaatadaptieve maatregelen personaliseren en prioriteren in de private ruimte. Hiermee maken wij de private ruimte waterrobuust, waterveilig, hitte- en droogtebestendig.</p>
+  <section>
+    <div class="inner-section">
+      <div class="horizontal-flex-row">
+        <div class="image-container">
+          <img class="mobile-image" src="assets/projects4.png" alt="">
+        </div>
+        <div class="text-container">
+          <h2>Gemeenten</h2>
+          <p class="bold-text">Wij kunnen elke bewoner in uw gemeente een persoonlijk klimaatadaptief advies geven zonder elke tuin in te gaan.</p>
+          <p>Wij gebruiken ruimtelijke data en sociale kennis uit de buurt om de gemeente gedetailleerd in kaart te brengen. In combinatie met veldwerk kunnen wij op postcode, buurt of wijkniveau klimaatadaptieve maatregelen personaliseren en prioriteren in de private ruimte. Hiermee maken wij de private ruimte waterrobuust, waterveilig, hitte- en droogtebestendig.</p>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section>
-  <div class="inner-section">
-    <div class="horizontal-flex-row">
-      <div class="text-container">
-        <h2>Bedrijventerreinen</h2>
-        <p class="bold-text">Bedrijventerreinen zijn vaak versteend, privaat terrein met veel oppervlakte.</p>
-        <p>SPATwater richt zich op het activeren van werknemers en bedrijven door vraag en aanbod op het gebied van water bij elkaar te brengen door middel van ruimtelijke data en geeft handvatten om bedrijventerreinen te vergroenen.</p>
-        <ul>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van stakeholders/netwerk analyses</li>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Co-creatie sessie organiseren</li>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van restwaterscans</li>
-          <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van klimaatanalyses en het koppelen van relevante maatregelen</li>
-        </ul>
-      </div>
-      <div class="image-container">
-        <img src="assets/verg.png" alt="">
+  <section>
+    <div class="inner-section">
+      <div class="horizontal-flex-row">
+        <div class="text-container">
+          <h2>Bedrijventerreinen</h2>
+          <p class="bold-text">Bedrijventerreinen zijn vaak versteend, privaat terrein met veel oppervlakte.</p>
+          <p>SPATwater richt zich op het activeren van werknemers en bedrijven door vraag en aanbod op het gebied van water bij elkaar te brengen door middel van ruimtelijke data en geeft handvatten om bedrijventerreinen te vergroenen.</p>
+          <ul>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van stakeholders/netwerk analyses</li>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Co-creatie sessie organiseren</li>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van restwaterscans</li>
+            <li><i class="fa fa-arrow-right" aria-hidden="true"></i> Het uitvoeren van klimaatanalyses en het koppelen van relevante maatregelen</li>
+          </ul>
+        </div>
+        <div class="image-container">
+          <img src="assets/verg.png" alt="">
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+</div>
 
 <style>
   section {
@@ -66,6 +68,11 @@
 
   .first-section {
     margin-top: 5rem;
+  }
+
+  .light-background {
+    background: var(--lg-bg);
+    padding: 1rem 0rem 2rem 0rem;
   }
 
   .inner-section {
