642cfc49997beb5fcd9bf27f45c4616321a0f1c5
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-10-12 17:41:57 +0200

    merch conflict fixed

--- src/lib/components/Klimaatadaptatie.svelte
+++ src/lib/components/Klimaatadaptatie.svelte
@@ -3,7 +3,6 @@
 
 <!-- Klimaat section -->
 
-<<<<<<< Updated upstream
 <div class="light-background">
   <section class="first-section">
     <div class="inner-section">
@@ -21,135 +20,10 @@
         <div class="image-container">
           <img src="assets/projects1.png" alt="">
         </div>
-=======
-<section
-  class="climate-container"
->
-  <div
-    class="inner-section"
-  >
-    <div
-      class="horizontal-flex-row"
-    >
-      <div
-        class="text-container"
-      >
-        <h2>
-          Klimaatadaptatie
-        </h2>
-        <p
-          class="bold-text"
-        >
-          Als meer
-          dan 60%
-          van een
-          gemeente
-          private
-          grond is…
-          dan is het
-          noodzakelijk
-          om deze
-          grond
-          klimaatadaptief
-          te maken.
-        </p>
-        <p>
-          Ons
-          vertrekpunt
-          is dat een
-          groot deel
-          van een
-          gemeente
-          bestaat
-          uit
-          private
-          grond
-          (tuinen,
-          daken,
-          bedrijventerreinen)
-          en dat het
-          absoluut
-          noodzakelijk
-          is dat
-          deze
-          private
-          ruimte
-          klimaatadaptief
-          ingericht
-          moet
-          worden.
-          Elk
-          perceel
-          vraagt om
-          maatwerk,
-          maar het
-          is
-          onmogelijk
-          om elke
-          tuin in te
-          gaan.
-        </p>
-        <ul>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Hoe los
-            je dit probleem
-            op?
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Hoe zet
-            je subsidiestromen
-            efficiënt
-            in?
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Hoe activeer
-            je bewoners?
-          </li>
-        </ul>
-      </div>
-      <div
-        class="image-container"
-      >
-        <img
-          src="assets/projects1.png"
-          alt=""
-        />
->>>>>>> Stashed changes
       </div>
     </div>
   </section>
 
-<<<<<<< Updated upstream
   <section>
     <div class="inner-section">
       <div class="horizontal-flex-row">
@@ -161,95 +35,10 @@
           <p class="bold-text">Wij kunnen elke bewoner in uw gemeente een persoonlijk klimaatadaptief advies geven zonder elke tuin in te gaan.</p>
           <p>Wij gebruiken ruimtelijke data en sociale kennis uit de buurt om de gemeente gedetailleerd in kaart te brengen. In combinatie met veldwerk kunnen wij op postcode, buurt of wijkniveau klimaatadaptieve maatregelen personaliseren en prioriteren in de private ruimte. Hiermee maken wij de private ruimte waterrobuust, waterveilig, hitte- en droogtebestendig.</p>
         </div>
-=======
-<section
-  class="climate-container"
->
-  <div
-    class="inner-section"
-  >
-    <div
-      class="horizontal-flex-row"
-    >
-      <div
-        class="image-container"
-      >
-        <img
-          class="mobile-image"
-          src="assets/projects4.png"
-          alt=""
-        />
-      </div>
-      <div
-        class="text-container"
-      >
-        <h2>
-          Gemeenten
-        </h2>
-        <p
-          class="bold-text"
-        >
-          Wij kunnen
-          elke
-          bewoner in
-          uw
-          gemeente
-          een
-          persoonlijk
-          klimaatadaptief
-          advies
-          geven
-          zonder
-          elke tuin
-          in te
-          gaan.
-        </p>
-        <p>
-          Wij
-          gebruiken
-          ruimtelijke
-          data en
-          sociale
-          kennis uit
-          de buurt
-          om de
-          gemeente
-          gedetailleerd
-          in kaart
-          te
-          brengen.
-          In
-          combinatie
-          met
-          veldwerk
-          kunnen wij
-          op
-          postcode,
-          buurt of
-          wijkniveau
-          klimaatadaptieve
-          maatregelen
-          personaliseren
-          en
-          prioriteren
-          in de
-          private
-          ruimte.
-          Hiermee
-          maken wij
-          de private
-          ruimte
-          waterrobuust,
-          waterveilig,
-          hitte- en
-          droogtebestendig.
-        </p>
->>>>>>> Stashed changes
       </div>
     </div>
   </section>
 
-<<<<<<< Updated upstream
   <section>
     <div class="inner-section">
       <div class="horizontal-flex-row">
@@ -267,131 +56,6 @@
         <div class="image-container">
           <img src="assets/verg.png" alt="">
         </div>
-=======
-<section
-  class="climate-container"
->
-  <div
-    class="inner-section"
-  >
-    <div
-      class="horizontal-flex-row"
-    >
-      <div
-        class="text-container"
-      >
-        <h2>
-          Bedrijventerreinen
-        </h2>
-        <p
-          class="bold-text"
-        >
-          Bedrijventerreinen
-          zijn vaak
-          versteend,
-          privaat
-          terrein
-          met veel
-          oppervlakte.
-        </p>
-        <p>
-          SPATwater
-          richt zich
-          op het
-          activeren
-          van
-          werknemers
-          en
-          bedrijven
-          door vraag
-          en aanbod
-          op het
-          gebied van
-          water bij
-          elkaar te
-          brengen
-          door
-          middel van
-          ruimtelijke
-          data en
-          geeft
-          handvatten
-          om
-          bedrijventerreinen
-          te
-          vergroenen.
-        </p>
-        <ul>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Het uitvoeren
-            van stakeholders/netwerk
-            analyses
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Co-creatie
-            sessie organiseren
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Het uitvoeren
-            van restwaterscans
-          </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="12"
-              viewBox="0 0 22 12"
-              fill="none"
-              ><path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75H21V5.25H0V6.75Z"
-                fill="black"
-              /></svg
-            > Het uitvoeren
-            van klimaatanalyses
-            en het koppelen
-            van relevante
-            maatregelen
-          </li>
-        </ul>
-      </div>
-      <div
-        class="image-container"
-      >
-        <img
-          src="assets/verg.png"
-          alt=""
-        />
->>>>>>> Stashed changes
       </div>
     </div>
   </section>
@@ -420,14 +84,8 @@
 
   h2 {
     padding: 0rem;
-<<<<<<< Updated upstream
     color: var(--spat);
     margin-bottom: 1rem;
-=======
-    color: var(
-      --spat
-    );
->>>>>>> Stashed changes
   }
 
   .bold-text {
